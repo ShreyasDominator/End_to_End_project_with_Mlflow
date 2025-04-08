3b19b5124d2d50f03b681d1423708f8a5ceb2a78
ShreyasDominator <mt23mcs004@students.vnit.ac.in>
2025-04-08 21:06:43 +0530

    Data Transformation Done

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 from Mlflow_ML_project import logger
 from Mlflow_ML_project.pipeline.stage_01_Data_ingestion import DataIngestionTrainingPipeline
 from Mlflow_ML_project.pipeline.stage_02_Data_Validation import DataValidationTrainingPipeline
+from Mlflow_ML_project.pipeline.stage_03_data_Transformation import DataTransformationTrainingPipeline
+
 
 logger.info('Welcome to our custom logging')
 STAGE_NAME=" Data Ingestion stage"
@@ -18,6 +20,16 @@ STAGE_NAME=" Data Validation stage"
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     obj = DataValidationTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME=" Data Transformation stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataTransformationTrainingPipeline()
     obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
