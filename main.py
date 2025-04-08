54908dea697739e5321c19882754e1344e07d212
ShreyasDominator <mt23mcs004@students.vnit.ac.in>
2025-04-08 11:50:29 +0530

    Data Validation done

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 # first adding the data-ingestion pipeline
 from Mlflow_ML_project import logger
 from Mlflow_ML_project.pipeline.stage_01_Data_ingestion import DataIngestionTrainingPipeline
+from Mlflow_ML_project.pipeline.stage_02_Data_Validation import DataValidationTrainingPipeline
 
 logger.info('Welcome to our custom logging')
 STAGE_NAME=" Data Ingestion stage"
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME=" Data Validation stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataValidationTrainingPipeline()
     obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
